77ab0472c341bc168bf39ae0118bddbaa4569591
Nate <Nate@LAPTOP-MLDAFOJL>
2018-10-13 21:23:31 -0600

    222
    Change-Id: Iad3149da373b7968b5f3f4f24d4be2aad777a5d2

--- CIT360/src/Threadsdemo/thread.java
+++ CIT360/src/Threadsdemo/thread.java
@@ -5,7 +5,7 @@ package Threadsdemo;
 public class thread extends Thread{
      public void run() {
         System.out.println("Another thread "+Thread.currentThread().getName()
-                + "extending Thread class!");
+                + " extending Thread class!");
     }
 
     
