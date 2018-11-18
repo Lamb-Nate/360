0f2b0051d051a62de44e27b08a3acd74f45e49ed
Nate <Nate@LAPTOP-MLDAFOJL>
2018-11-17 17:30:30 -0700

    777

---
+++ src/java/MyServlet.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Nate
+ */
+public class MyServlet extends HttpServlet {
+
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            out.println("<h2> Welcome to my first servlet application in Netbeans</h2>");
+            
+        } finally {
+            out.close();
+        }
+    }
+   
+    }
+
+    
+
